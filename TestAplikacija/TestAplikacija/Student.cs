d3af1ca7a8dbd5fcb60a9521acad37ec3e0a0cc0
dzoni007 <dukic291@gmail.com>
2022-11-02 16:02:23 +0100

    Default program

---
+++ TestAplikacija/TestAplikacija/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAplikacija
+{
+    internal class Student
+    {
+        private string ime;
+        private int godine;
+
+        public Student(string ime, int godine)
+        {
+            this.Ime = ime;
+            this.Godine = godine;
+        }
+
+        public string Ime { get => ime; set => ime = value; }
+        public int Godine { get => godine; set => godine = value; }
+
+        public override string? ToString()
+        {
+            return "Ime: "+Ime+" Godine: "+Godine;
+        }
+
+
+        
+
+    }
+}
---
+++ TestAplikacija/cs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Person
+{
+	string ime;
+	string prezime;
+	int godine;
+	public Person(global::System.String ime, global::System.String prezime)
+	{
+		this.ime = ime;
+		this.prezime = prezime;
+	}
+	public static void ispis()
+	{
+		global::System.Console.WriteLine("ovo je globalno");
+	}
+
+	
+
+
+
+}
